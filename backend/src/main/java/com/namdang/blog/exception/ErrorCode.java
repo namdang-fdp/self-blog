305a475e0abaa16e15baddc2acb483a076d67b9c
namdang-fdp <namdangcoder@gmail.com>
2025-09-10 15:53:27 +0700

    feat: add new error code

--- backend/src/main/java/com/namdang/blog/exception/ErrorCode.java
+++ backend/src/main/java/com/namdang/blog/exception/ErrorCode.java
@@ -6,7 +6,11 @@ import org.springframework.http.HttpStatusCode;
 
 @Getter
 public enum ErrorCode {
-    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR);
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_ALREADY_EXIST(1001, "User already exist", HttpStatus.BAD_REQUEST),
+    USER_NOT_FOUND(1002, "User not found", HttpStatus.BAD_REQUEST),
+    USER_NOT_ENABLE(1003, "User not enable", HttpStatus.BAD_REQUEST),
+    ;
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
