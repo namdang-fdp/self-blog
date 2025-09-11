3ee63d1fb2902e5a9d33a7cda31c6df2394ba708
namdang-fdp <namdangcoder@gmail.com>
2025-09-11 13:09:10 +0700

    feat: add blog entity

---
+++ backend/src/main/java/com/namdang/blog/entities/BlogEntity.java
@@ -0,0 +1,51 @@
+package com.namdang.blog.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "blogs")
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlogEntity {
+    @Id
+    @GeneratedValue
+    private UUID blogId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "posted_user_id", nullable = false)
+    private UserEntity postedUser;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String summary;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String coverPath;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date publicationDate;
+
+    @ManyToMany
+    @JoinTable(
+            name = "blog_genres",
+            joinColumns = @JoinColumn(name = "blogId"),
+            inverseJoinColumns = @JoinColumn(name = "genresId")
+    )
+    public List<GenresEntity> genres;
+
+    private int view;
+
+    private int totalReact;
+
+}
