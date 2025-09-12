6ea09ec233bf16084541aab30337476f6a834e22
namdang-fdp <namdangcoder@gmail.com>
2025-09-12 09:52:38 +0700

    feat: add genre response

---
+++ backend/src/main/java/com/namdang/blog/dto/response/GenreResponse.java
@@ -0,0 +1,15 @@
+package com.namdang.blog.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenreResponse {
+    private String genre;
+}
