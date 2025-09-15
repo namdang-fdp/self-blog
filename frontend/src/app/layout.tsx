b20b0e1debc22fb1de9d98f12ee0847c4921ea06
namdang-fdp <namdangcoder@gmail.com>
2025-09-15 20:08:05 +0700

    chore: refactor to client and root layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import ClientLayout from './client-layout';
 import { Header } from '@/components/header';
-
-const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
     title: 'Dorriss Blog',
@@ -20,21 +10,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
     children,
-}: Readonly<{
+}: {
     children: React.ReactNode;
-}>) {
+}) {
     return (
-        <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <Header />
-                <div className="min-h-screen flex flex-col">
-                    <main className="flex-1 flex flex-col min-w-full">
-                        {children}
-                    </main>
-                </div>
-            </body>
-        </html>
+        <ClientLayout>
+            <Header />
+            {children}
+        </ClientLayout>
     );
 }
