3c73cbcd6d3f3e31bbcec752445a1743ca0d1bc7
namdang-fdp <namdangcoder@gmail.com>
2025-09-15 13:12:00 +0700

    feat: api to post blog

--- backend/src/main/java/com/namdang/blog/controllers/BlogController.java
+++ backend/src/main/java/com/namdang/blog/controllers/BlogController.java
@@ -1,5 +1,6 @@
 package com.namdang.blog.controllers;
 
+import com.namdang.blog.dto.request.BlogRequest;
 import com.namdang.blog.dto.response.ApiResponse;
 import com.namdang.blog.dto.response.BlogResponse;
 import com.namdang.blog.services.BlogService;
@@ -8,9 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -29,6 +29,15 @@ public class BlogController {
         ApiResponse<Page<BlogResponse>> apiResponse = new ApiResponse<>();
         apiResponse.setMessage("Get all blogs successfully");
         apiResponse.setResult(result);
+        return apiResponse;
+    }
+
+    @PostMapping(value = "/blog", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(description = "Post new blog")
+    public ApiResponse<Void> createNewBlog(@ModelAttribute BlogRequest blogRequest) {
+        blogService.createBlog(blogRequest);
+        ApiResponse<Void> apiResponse = new ApiResponse<>();
+        apiResponse.setMessage("Create new blog successfully");
         return apiResponse;
     }
 
