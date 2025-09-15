3b7518190b829aff292df28dad50373edb07c247
namdang-fdp <namdangcoder@gmail.com>
2025-09-15 18:05:23 +0700

    feat(blog): add find by id service

--- backend/src/main/java/com/namdang/blog/services/BlogService.java
+++ backend/src/main/java/com/namdang/blog/services/BlogService.java
@@ -74,4 +74,11 @@ public class BlogService {
 
         blogRepository.save(blog);
     }
+
+    public BlogResponse getBlogById(UUID id) {
+        BlogEntity blogEntity = blogRepository.findByBlogId(id)
+                .orElseThrow(() -> new AppException(ErrorCode.BLOG_NOT_FOUND));
+        return
+
+    }
 }
