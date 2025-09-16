c6bb269c5eeb2dda3b8b6c55c9b9adcbdf52620c
namdang-fdp <namdangcoder@gmail.com>
2025-09-16 11:40:42 +0700

    feat: config for get image from s3

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     output: 'standalone',
+    images: {
+        domains: ['dorriss-blog-bucket.s3.ap-southeast-2.amazonaws.com'],
+    },
 };
 
 export default nextConfig;
