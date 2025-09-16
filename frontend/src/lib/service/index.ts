6ebcd13975bcf5fb0ad3678c1933b471871c03d5
namdang-fdp <namdangcoder@gmail.com>
2025-09-16 09:51:37 +0700

    feat: config fetchwrapper

---
+++ frontend/src/lib/service/index.ts
@@ -0,0 +1,33 @@
+import {
+    UndefinedInitialDataOptions,
+    UseQueryResult,
+} from '@tanstack/react-query';
+import { getApiUrl } from './api-url';
+
+export class ApiError extends Error {
+    details: Record<string, string>;
+
+    constructor(message: string, details: Record<string, string>) {
+        super(message);
+        this.details = details;
+    }
+}
+
+export const throwIfError = async (response: Response) => {
+    if (!response.ok) {
+        const error: ApiError = await response.json();
+        throw error;
+    }
+};
+
+export const fetchWrapper = async (
+    url: RequestInfo | URL,
+    init?: RequestInit,
+) => {
+    const base = await getApiUrl();
+    const apiUrl = `${base}${url}`;
+    return await fetch(apiUrl, {
+        ...init,
+        credentials: 'include',
+    });
+};
